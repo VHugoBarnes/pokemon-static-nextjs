d37a5e0257e86918e697108550399e746151abc6
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-04 16:23:19 -0600

    add title prop to Layout comp

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,16 +1,16 @@
-import { ReactNode } from "react";
+import { FC, ReactNode } from "react";
 import Head from "next/head";
 
 type PropType = { children: ReactNode, title?: string };
 
-export const Layout = ({ children, title }: PropType) => {
+export const Layout: FC<PropType> = ({ children, title }: PropType) => {
   return (
     <>
       <Head>
         <title>{`${title} | Pokemon App` ?? "Pokemon App"}</title>
         <meta name="author" content="Victor Vazquez" />
-        <meta name="description" content="Info about Pokemon X" />
-        <meta name="keywords" content="X, pokemon, pokedex" />
+        <meta name="description" content={`Info about Pokemon ${title}`} />
+        <meta name="keywords" content={`${title}, pokemon, pokedex`} />
       </Head>
 
       <main>
