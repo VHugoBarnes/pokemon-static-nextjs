7de9af8d3a2b302091f5caa5fe95e39891301e67
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-05 16:36:17 -0600

    Links in navbar works

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -5,21 +5,23 @@ import Image from "next/image";
 export const Navbar = () => {
   return (
     <NextNavbar>
-      <NavbarBrand>
-        <Image
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-          alt="ditto sprite"
-          width={80}
-          height={80}
-          className="w-20 h-20"
-          priority
-        />
-        <h2 className="text-3xl">P</h2>
-        <h3 className="text-xl">okemon</h3>
+      <NavbarBrand >
+        <Link href="/" color="foreground">
+          <Image
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+            alt="ditto sprite"
+            width={80}
+            height={80}
+            className="w-20 h-20"
+            priority
+          />
+          <h2 className="text-3xl">P</h2>
+          <h3 className="text-xl">okemon</h3>
+        </Link>
       </NavbarBrand>
       <NavbarContent justify="end">
         <NavbarItem>
-          <Link>Favoritos</Link>
+          <Link href="/favorites">Favoritos</Link>
         </NavbarItem>
       </NavbarContent>
     </NextNavbar>
---
+++ src/pages/favorites/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Favorites = () => {
+  return (
+    <div>index</div>
+  );
+};
+
+export default Favorites;
